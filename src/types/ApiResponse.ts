b40d5b0557f598608e4ee537f68ebee54c26fc8d
Shubh Verma <shubhverma2003@gmail.com>
2025-02-17 21:48:06 +0530

    added comments

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,8 +1,12 @@
+// Importing the Message type from the User model
 import { Message } from "@/model/User";
 
+/**
+ * ApiResponse interface defines the structure of responses returned from API functions.
+ */
 export interface ApiResponse {
-  success: boolean;
-  message: string;
-  isAcceptingMessages?: boolean;
-  messages?: Array<Message>;
+  success: boolean; // Indicates whether the API request was successful (true) or failed (false)
+  message: string; // Contains a success or error message describing the API response
+  isAcceptingMessages?: boolean; // (Optional) Tells whether the user is accepting messages
+  messages?: Array<Message>; // (Optional) Contains an array of messages if applicable
 }
