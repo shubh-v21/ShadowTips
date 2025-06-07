8439bac7843de37af33fa6558406da4db37ebd45
Shubh Verma <shubhverma2003@gmail.com>
2025-06-07 23:50:46 +0530

    remove resend.ts file

--- src/lib/resend.ts
+++
@@ -1,6 +0,0 @@
-// Import the Resend class from the "resend" package
-import { Resend } from "resend";
-
-// Create an instance of Resend with the API key from environment variables
-// This API key is stored in the .env file for security reasons
-export const resend = new Resend(process.env.RESEND_API_KEY);
