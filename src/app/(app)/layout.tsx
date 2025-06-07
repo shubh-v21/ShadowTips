0bc8bba7e7d39a6c46cfd9d8a920bebe8afaa421
Shubh Verma <shubhverma2003@gmail.com>
2025-06-07 23:51:53 +0530

    update title in metadata

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "True Feedback",
+  title: "ShadowTips",
   description: "Real feedback from real people.",
 };
 
