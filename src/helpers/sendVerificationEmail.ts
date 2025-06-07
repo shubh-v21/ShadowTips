76ede1f034298afa8fc5d26e678aacfd894b0f64
Shubh Verma <shubhverma2003@gmail.com>
2025-06-07 23:51:18 +0530

    refactor sendVerificationEmail to use nodemailer instead of Resend API

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,47 +1,31 @@
-// Import the "resend" instance from the custom resend configuration file
-import { resend } from "@/lib/resend";
-
-// Import the VerificationEmail component, which is used to send a styled email
+import sendEmail from "@/lib/nodemailerUtility";
 import VerificationEmail from "../../emails/VerificationEmail";
-
-// Import the ApiResponse type to define the return type of the function
 import { ApiResponse } from "@/types/ApiResponse";
+import { render } from "@react-email/render";
 
-/**
- * Sends a verification email to the user using Resend API.
- *
- *  email - The recipient's email address.
- *  username - The username of the recipient.
- *  verifyCode - The verification code to be included in the email.
- * returns A promise that resolves to an ApiResponse object indicating success or failure.
- */
 export async function sendVerificationEmail(
   email: string,
   username: string,
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
-    // Send an email using the Resend API
-    await resend.emails.send({
-      from: "onboarding@resend.dev", // Sender email address
-      to: email, // Recipient's email address
-      subject: "True Feedback | Verification Code", // Email subject
-      react: VerificationEmail({ username, otp: verifyCode }), // Renders a React-based email template
-    });
+    const subject = "Verify your email";
+    const emailHtml = await render(VerificationEmail({ username, otp: verifyCode }));
 
-    // Return a success response if the email is sent successfully
+    await sendEmail(email, subject, emailHtml);
+
     return {
       success: true,
-      message: "Verification email sent successfully",
+      message: "OTP sent successfully",
     };
-  } catch (emailError) {
-    // Log the error if email sending fails
-    console.error("Error sending verification email", emailError);
+  } catch (error) {
+    console.error("Error sending OTP:", error);
 
-    // Return a failure response if email sending fails
     return {
       success: false,
-      message: "Failed to send verification email",
+      message: "Failed to send OTP",
     };
   }
 }
+
+export default sendVerificationEmail;
